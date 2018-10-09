e69330f8f66fed0cb6c8edd78a79fea1acaf0831
samih1079 <sabbas79>
2018-10-09 11:16:09 +0300

    Initial commit

---
+++ app/src/main/java/com/abbas/samih/samihtaskmngr2018/SpalshActivity.java
@@ -0,0 +1,39 @@
+package com.abbas.samih.samihtaskmngr2018;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpalshActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spalsh);
+
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        MyThread myThread=new MyThread();
+        myThread.start();
+        super.onResume();
+    }
+
+    public  class MyThread extends Thread
+    {
+        @Override
+        public void run() {
+
+            try {
+                sleep(3000);
+                Intent i=new Intent(getApplicationContext(),MainTabsActivity.class);
+                startActivity(i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
