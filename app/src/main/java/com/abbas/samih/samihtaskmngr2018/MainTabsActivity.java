c65faf3d96a4db57bed7b9b03eb9c55ef19484bd
samih1079 <sabbas79>
2018-10-23 13:26:14 +0300

    2. finshing tabs empty fragment

--- app/src/main/java/com/abbas/samih/samihtaskmngr2018/MainTabsActivity.java
+++ app/src/main/java/com/abbas/samih/samihtaskmngr2018/MainTabsActivity.java
@@ -66,6 +66,7 @@ public class MainTabsActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
             }
         });
 
